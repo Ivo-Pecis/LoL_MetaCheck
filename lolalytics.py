2219428b950382118c4f976826f9768920b8a55e
Ivo-Pecis <ivo.pecis@edu.rtu.lv>
2024-01-17 13:09:11 +0200

    Masively upgraded LoLalytics speed

--- lolalytics.py
+++ lolalytics.py
@@ -1,13 +1,12 @@
-from tier_values import tier_values_LoLalytics, average_tier_value
-
 import selenium
+from tier_values import tier_values_LoLalytics, average_tier_value
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
-from openpyxl import Workbook as wb, load_workbook
 from pynput.keyboard import Key, Controller
+from openpyxl import Workbook as wb, load_workbook 
 import time
+from pynput.keyboard import Key, Controller
 
 service = Service()
 option = webdriver.ChromeOptions()
@@ -15,7 +14,21 @@ keyboard = Controller()
 
 driver = webdriver.Chrome(service=service, options=option)
 
-url = "https://www.lolalytics.com"
+top_tier_list = []
+jg_tier_list = []
+mid_tier_list = []
+adc_tier_list = []
+supp_tier_list = []
+
+role_tier_lists = {
+    'top': top_tier_list,
+    'jungle': jg_tier_list, 
+    'middle': mid_tier_list, 
+    'bottom': adc_tier_list, 
+    'support': supp_tier_list, 
+}
+
+url = f"https://lolalytics.com/lol/tierlist/?lane=Top"
 driver.get(url)
 time.sleep(2)
 
@@ -27,41 +40,94 @@ find = driver.find_element(By.CLASS_NAME, "ncmp__btn.ncmp__btn-danger")
 find.click()
 time.sleep(2)
 
+for role, tier_list in role_tier_lists.items():
+    url = f"https://lolalytics.com/lol/tierlist/?lane={role}"
+    driver.get(url)
+    time.sleep(2)
+
+    elements = driver.find_elements(By.CLASS_NAME, "TierList_list__j33gd")
+    if elements:
+            for i in range(len(elements)):
+                first_element = elements[i]
+                tier = first_element.text.split("\n")
+                for j in range(len(tier)):
+                    tiers = []
+                    if tier[j][:1].isalpha():
+                        if tier[j] != 'I' and tier[j] != "II" and tier[j] != "III" and tier[j] != "IV":
+                            champion_atributes = tier[j].split(" ")
+                            if len(champion_atributes) == 2:
+                                champion_name = champion_atributes[0]
+                                champion_tier = champion_atributes[1]
+                            elif len(champion_atributes) == 3:
+                                champion_name = champion_atributes[0] + " " + champion_atributes[1]
+                                champion_tier = champion_atributes[2]
+                            tiers.append(champion_name)
+                            tiers.append(champion_tier)
+                            tiers.append(tier_values_LoLalytics(champion_tier))
+                            tier_list.append(tiers)
+top_tier_list.sort(key=lambda x: x[0])
+jg_tier_list.sort(key=lambda x: x[0])
+mid_tier_list.sort(key=lambda x: x[0])
+adc_tier_list.sort(key=lambda x: x[0])
+supp_tier_list.sort(key=lambda x: x[0])
+print(mid_tier_list)
 with open('LoLChampions.txt') as f:
-    contents = f.read().replace('\n', ' ').split(",")
-tiers = []
-for i in contents:
-    find=driver.find_element(By.CLASS_NAME, "SearchBar_search__BJeOt")
-    find.click()
-
-    keyboard.type(i.replace("'", ""))
-    keyboard.press(Key.enter)
-    time.sleep(1)
-
-    find=driver.find_element(By.CLASS_NAME, "CircleBig_wrapper__GgAW5")
-    tier = find.find_element(By.CLASS_NAME, "CircleBig_tier__8AcED").text
-    tiers.append(tier)
-
-champion_stats= []
-all_tier_values = []
-for i in range (len(contents)):
-    all_tier_values.append(tier_values_LoLalytics(tiers[i]))   
-average_tier = average_tier_value(all_tier_values)
-for i in range (len(contents)):
-    champion= []
-    tier_value = tier_values_LoLalytics(tiers[i])
-    champion.append(contents[i]) 
-    champion.append(tiers[i])
-    champion.append(tier_value)
-    champion.append(tier_value/average_tier) 
-    champion_stats.append(champion)
-
+    contents = f.read().replace('\n', '').split(",")
 wb=load_workbook('LoLChampions.xlsx')
-ws = wb.active
-for i in range (len(champion_stats)):
-    for j in range (len(champion_stats[i])):
-        ws.cell(row=i+2, column=j+1).value = champion_stats[i][j]
+ws=wb['TOP']
+count = 0
+for i in range(0,len(contents)):    
+    if contents[i] == (top_tier_list[count][0]).upper():
+        ws.cell(row=i+2, column=8).value = top_tier_list[count][1]
+        ws.cell(row=i+2, column=9).value = top_tier_list[count][2]
+        if count < len(top_tier_list)-1:
+            count += 1
+    else:
+        ws.cell(row=i+2, column=8).value = "Not Found"
+        ws.cell(row=i+2, column=9).value = 0
+count = 0
+ws=wb['JG']
+for i in range(0,len(contents)):
+    if contents[i] == (jg_tier_list[count][0]).upper():
+        ws.cell(row=i+2, column=8).value = jg_tier_list[count][1]
+        ws.cell(row=i+2, column=9).value = jg_tier_list[count][2]
+        if count < len(jg_tier_list)-1:
+            count += 1
+    else:
+        ws.cell(row=i+2, column=8).value = "Not Found"
+        ws.cell(row=i+2, column=9).value = 0
+ws=wb['MID']
+count = 0
+for i in range(0,len(contents)): 
+    if contents[i] == (mid_tier_list[count][0]).upper():
+        ws.cell(row=i+2, column=8).value = mid_tier_list[count][1]
+        ws.cell(row=i+2, column=9).value = mid_tier_list[count][2]
+        if count < len(mid_tier_list)-1:
+            count += 1
+    else:
+        ws.cell(row=i+2, column=8).value = "Not Found"
+        ws.cell(row=i+2, column=9).value = 0
+ws=wb['ADC']
+count = 0
+for i in range(0,len(contents)):
+    if contents[i] == (adc_tier_list[count][0]).upper():
+        ws.cell(row=i+2, column=8).value = adc_tier_list[count][1]
+        ws.cell(row=i+2, column=9).value = adc_tier_list[count][2]
+        if count < len(adc_tier_list)-1:
+            count += 1
+    else:
+        ws.cell(row=i+2, column=8).value = "Not Found"
+        ws.cell(row=i+2, column=9).value = 0
+ws=wb['SUPP']
+count = 0
+for i in range(0,len(contents)):
+    if contents[i] == (supp_tier_list[count][0]).upper():
+        ws.cell(row=i+2, column=8).value = supp_tier_list[count][1]
+        ws.cell(row=i+2, column=9).value = supp_tier_list[count][2]
+        if count < len(supp_tier_list)-1:
+            count += 1
+    else:
+        ws.cell(row=i+2, column=8).value = "Not Found"
+        ws.cell(row=i+2, column=9).value = 0
 wb.save('LoLChampions.xlsx')
 wb.close()
-
-print(average_tier_value(all_tier_values))
